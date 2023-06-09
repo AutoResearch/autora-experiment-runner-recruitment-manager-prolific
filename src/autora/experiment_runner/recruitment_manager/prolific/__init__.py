3c665e3b970da445677890b3931b8d0ca8218089
Younes Strittmatter <younes_strittmatter@brown.edu>
2023-06-09 14:38:50 -0400

    feat: action to publish study

--- src/autora/experiment_runner/recruitment_manager/prolific/__init__.py
+++ src/autora/experiment_runner/recruitment_manager/prolific/__init__.py
@@ -235,6 +235,12 @@ def start_study(study_id: str, prolific_token: str):
     """
     return _update_study_status(study_id, "START", prolific_token)
 
+def publish_study(study_id: str, prolific_token: str):
+    """
+    Publish the study
+    """
+    return _update_study_status(study_id, "PUBLISH", prolific_token)
+
 
 class EligibilityOptions:
     @staticmethod
